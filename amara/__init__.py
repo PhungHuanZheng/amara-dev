4faf27034bc786d944eb6946b6ef15779900f9f3
PhungHuanZheng <intern1.amara@gmail.com>
2023-06-14 14:23:08 +0800

    updated doc for amara, TOfinish SupervisedModelSelector

--- amara/__init__.py
+++ amara/__init__.py
@@ -1,10 +1,14 @@
 """
-Main functionality module for Amara Hotels data science and analytics
+Amara
+-----
+Main functionality module for Amara Hotels data science and analytics. Provides 
+centralization for all functions, classes and other utility used by Amara Hotels
+data processing and presentation. This module currently acts as a basic infrastructure
+for a larger module to be built upon in the future while still providiing functionality.
 
 Author(s)
 ---------
-Phung Huan Zheng: huanzhengphung@gmail.com
-                  intern@amarahotels.com
+Phung Huan Zheng (13-Mar-2023 - 11-Aug-2023): 
 
-                  13-Mar-2023 - 11-Aug-2023
+huanzhengphung@gmail.com | intern@amarahotels.com
 """
--- amara/machinelearning/utils/SupervisedModelSelector.py
+++ amara/machinelearning/utils/SupervisedModelSelector.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import Literal
+import time
 
 import numpy as np
 import pandas as pd
@@ -16,19 +17,25 @@ from sklearn.svm import SVC, SVR
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier, SGDRegressor, ElasticNet, BayesianRidge
 
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error, r2_score
 
 
 class SupervisedModelSelector:
-    __classification_models = [KNeighborsClassifier, DecisionTreeClassifier, RandomForestClassifier, GradientBoostingClassifier, LogisticRegression, GaussianNB, SGDClassifier, SVC]
-    __regression_models = [KNeighborsRegressor, DecisionTreeRegressor, GradientBoostingRegressor, RandomForestRegressor, SVR, LogisticRegression, SGDRegressor, ElasticNet, BayesianRidge]
+    __classification_models = [KNeighborsClassifier(), DecisionTreeClassifier(), RandomForestClassifier(), GradientBoostingClassifier(), LogisticRegression(), GaussianNB(), SGDClassifier(), SVC()]
+    __regression_models = [KNeighborsRegressor(), DecisionTreeRegressor(), GradientBoostingRegressor(), RandomForestRegressor(), SVR(), LogisticRegression(), SGDRegressor(), ElasticNet(), BayesianRidge()]
+
+    __classification_metrics = [accuracy_score, precision_score, recall_score, f1_score]
+    __regression_metrics = [mean_absolute_error, mean_squared_error, mean_absolute_percentage_error, r2_score]
 
     def __init__(self, *, goal: Literal['classification', 'regression'], preprocessor: Pipeline | ColumnTransformer = None) -> None:
         # load valid models
         if goal == 'classification':
             self.__models = self.__classification_models
+            self.__metrics = self.__classification_metrics
         elif goal == 'regression':
             self.__models = self.__regression_models
+            self.__metrics = self.__regression_metrics
         else:
             raise ValueError(f'Keyword argument "goal" must be either "classification" or "regression", got "{goal}" instead.')
         
@@ -43,4 +50,40 @@ class SupervisedModelSelector:
         """
         
         """
-        pass
+
+        # get models
+        if models is None:
+            models = self.__models
+
+        # results as dataframe
+        results_df: dict[str, str | float] = {}
+
+        # iterate over models
+        for i, model in enumerate(models):
+            # build pipeline with model
+            pipeline = Pipeline([
+                ('preprocessor', self.__preprocessor),
+                ('estimator', model)
+            ])
+
+            # skip model if unable to train/fit/get metrics
+            try:
+                # record time taken to fit
+                start = time.perf_counter()
+
+                # fit to data
+                pipeline = pipeline.fit(X_train, y_train)
+                y_pred = pipeline.predict(X_test)
+                time_taken = time.perf_counter() - start
+
+                # get metrics
+                metrics = [str(model)] + [metric(y_test, y_pred, average='macro') for metric in self.__metrics] + [f'{time_taken:.2f}s']
+                results_df[i] = metrics
+
+            except Exception as e:
+                raise e
+            
+
+        return results_df
+
+            
