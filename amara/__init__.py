3956874cd27e788f06b8a2568afc0a14a9a40456
PhungHuanZheng <intern1.amara@gmail.com>
2023-06-14 10:08:40 +0800

    updated docs

---
+++ amara/__init__.py
@@ -0,0 +1,10 @@
+"""
+Main functionality module for Amara Hotels data science and analytics
+
+Author(s)
+---------
+Phung Huan Zheng: huanzhengphung@gmail.com
+                  intern@amarahotels.com
+
+                  13-Mar-2023 - 11-Aug-2023
+"""
