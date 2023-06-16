f2cb98a4884ec3092428f30080bc850d7a543846
PhungHuanZheng <intern1.amara@gmail.com>
2023-06-16 12:16:40 +0800

    removed departments from branches, no longer needed.

--- amara/static/branches.py
+++ amara/static/branches.py
@@ -6,10 +6,10 @@ location information, currency conversion, etc
 
 hotels = {
     'ASIN': {
-        'Agilysis': ['ELEMENT', 'ELEMENT @ TRAS', 'ROOM SVC', 'CAFE ORIENTAL', 'BANQUET', 'SILK ROAD', 'TEA ROOM', 'Amara Club Lounge', 'MiniBar']
+        
     },
     'ASRS': {
-        'Hero': ['SHUTTERS', 'BANQUET']
+        
     },
     'ABBK': {
 
