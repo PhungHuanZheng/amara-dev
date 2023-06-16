c678424a369e3804c00b8b07da06ce8cd4ba00cb
PhungHuanZheng <intern1.amara@gmail.com>
2023-06-16 10:33:48 +0800

    added integration for progress trackers

--- amara/core/parallel.py
+++ amara/core/parallel.py
@@ -12,8 +12,10 @@ from joblib.parallel import Parallel, delayed
 
 import pandas as pd
 
+from amara.visuals.progress import ThreadSafeProgressBar
 
-def processor_loop(filepath: os.PathLike | str, sheet_names: list[str] = None, processor: Callable[[pd.DataFrame], pd.DataFrame] = None) -> pd.DataFrame:
+
+def processor_loop(filepath: os.PathLike | str, sheet_names: list[str] = None, processor: Callable[[pd.DataFrame], pd.DataFrame] = None, progress_tracker: ThreadSafeProgressBar = None) -> pd.DataFrame:
     """
     Loop function to be used with the `joblib.Parallel` class for data processing
     with Pandas.
@@ -26,6 +28,9 @@ def processor_loop(filepath: os.PathLike | str, sheet_names: list[str] = None, p
         Sheetnames in the Excel file passed to be passed to the `processor`.
     `processor` : `Callable[[pd.DataFrame], pd.DataFrame]`, `default=None`
         Processor to be used on the DataFrame extracted before returning.
+    `progress_tracker` : `ThreadSafeProgressBar`
+        Progress bar object to track progress within job. Ensure that 
+        `backend='threading'` is set for the `Parallel` object.
 
     Returns
     -------
@@ -48,6 +53,12 @@ def processor_loop(filepath: os.PathLike | str, sheet_names: list[str] = None, p
         return processor(pd.read_excel(filepath))
 
     dfs: dict[str, pd.DataFrame] = pd.read_excel(filepath, sheet_name=sheet_names)
-    return pd.concat([processor(df) for _, df in dfs.items()])
+    df = pd.concat([processor(df) for _, df in dfs.items()])
+
+    if progress_tracker is not None:
+        progress_tracker.update()
+    
+    return df
+    
 
 
