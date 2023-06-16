82f6ab0ee2e9564393d35c325d378bce99c843c3
PhungHuanZheng <intern1.amara@gmail.com>
2023-06-16 09:26:10 +0800

    update __init__ file

--- amara/datasets/__init__.py
+++ amara/datasets/__init__.py
@@ -2,3 +2,5 @@
 This module provides functionality specific to datasets, such as mending of Arrival/
 Departure dates for ONLY Info HMS Raw Arrivals datasets.
 """
+
+from Master_Calendar import MasterCalendar
