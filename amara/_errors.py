68bd7c713cd69b510a0dc383062e8520710192aa
Intern Revenue <intern@amaragroup.com>
2023-06-19 16:02:17 +0800

    updated name

--- amara/_errors.py
+++ amara/_errors.py
@@ -2,7 +2,7 @@
 This module provides Exception types used throughout `amara`.
 """
 
-class NotInitiated(Exception):
+class NotInitiatedError(Exception):
     """
     Property of class exists but true value has not been initiated
     by another method.
